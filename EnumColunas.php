301fb2bd695b95822d8464c9eab9ad2b38f05e94
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-08-27 18:53:08 -0300

    Criada a pagina inicial

--- EnumColunas.php
+++ EnumColunas.php
@@ -1,23 +1,22 @@
 <?php
-//http://www.devwilliam.com.br/php/aprenda-como-ler-arquivos-xml-com-php
 
-class Coluna {
+	class Coluna {
 
-	const CATEGORIA = 0;
-  const MARCA = 1;
-	const FORNECEDOR = 2;
-	const CODIGO_NFE = 3;
-	const NUMERO_SERIE = 4;
-	const NOME = 5;
-	const EAN = 6;
-	const GRUPO_TRIBUTARIO = 7;
-	const NCM = 8;
-	const CFOP = 9;
-	const GENERO = 10;
-	const ESTOQUE_UNIDADE = 11;
-	const PRECO_CUSTO = 12;
-	const LUCRO_DINHEIRO = 13;
+		const CATEGORIA = 0;
+	  const MARCA = 1;
+		const FORNECEDOR = 2;
+		const CODIGO_NFE = 3;
+		const NUMERO_SERIE = 4;
+		const NOME = 5;
+		const EAN = 6;
+		const GRUPO_TRIBUTARIO = 7;
+		const NCM = 8;
+		const CFOP = 9;
+		const GENERO = 10;
+		const ESTOQUE_UNIDADE = 11;
+		const PRECO_CUSTO = 12;
+		const LUCRO_DINHEIRO = 13;
 
-}//fim da classe
+	}//fim da classe
 
 ?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<html>
+  <head>
+    <title>KIT FERRAMENTAS</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" type='text/css'>
+  </head>
+  <body class="conatiner">
+    <div class="row">
+      <div class="col-sm-3"></div><!--Fim da Col-->
+      <div class="col-sm-3">
+          <div class='panel panel-default'>
+            <div class='panel-body'>
+                <span>teste body</span>
+            </div>
+            <div class='panel-footer'>
+                <span>teste footer</span>
+            </div>
+          </div><!--Fim do Panel-->
+      </div><!--Fim da Col-->
+      <div class="col-sm-3"></div><!--Fim da Col-->
+    </div><!--Fim da Row-->
+  </body>
+</html>
