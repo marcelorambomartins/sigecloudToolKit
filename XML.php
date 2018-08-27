d6c5093cd5e1b74b9d1db14ccd3401776f3cc9fe
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-08-27 15:45:44 -0300

    Criado a funcção para capturar a quantidade do produto (XML)

--- XML.php
+++ XML.php
@@ -14,6 +14,19 @@ Class XML{
 
 		endforeach;
 	}//fim da funcao
+
+
+	static function obterQuantProduto($codigoProduto,$xml){
+
+		foreach($xml->NFe->infNFe->det as $produto):
+			$codXML = $produto->prod->cProd; //captura codigo do produto
+
+			if($codigoProduto == $codXML){
+				return $produto->prod->qTrib; //retorna quantidade do produto
+			}
+
+		endforeach;
+	}//fim da funcao
 }//fim da classe
 
 ?>
