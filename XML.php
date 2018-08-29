19eaa58ce653e6ca554ba32b63fc6787c5ed214c
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-08-29 11:24:43 -0300

    Criada a funcão de obter o preço do custo no XML

--- XML.php
+++ XML.php
@@ -1,15 +1,18 @@
 <?php
 //http://www.devwilliam.com.br/php/aprenda-como-ler-arquivos-xml-com-php
 
+
+# verificar a possibilidade de refatorar as funções de obter itens do XML
+
 Class XML{
 
 	static function obterInfoProduto($codigoProduto,$xml){
 
-		foreach($xml->NFe->infNFe->det as $produto):
-			$codXML = $produto->prod->cProd;
+		foreach($xml->NFe->infNFe->det as $produto): //percorre todos os produtos
+			$codXML = $produto->prod->cProd; //captura codigo do produto
 
 			if($codigoProduto == $codXML){
-				return $produto->infAdProd;
+				return $produto->infAdProd; //retorna a informacao adicional do produto
 			}
 
 		endforeach;
@@ -18,7 +21,7 @@ Class XML{
 
 	static function obterQuantProduto($codigoProduto,$xml){
 
-		foreach($xml->NFe->infNFe->det as $produto):
+		foreach($xml->NFe->infNFe->det as $produto): //percorre todos os produtos
 			$codXML = $produto->prod->cProd; //captura codigo do produto
 
 			if($codigoProduto == $codXML){
@@ -27,6 +30,20 @@ Class XML{
 
 		endforeach;
 	}//fim da funcao
+
+
+	static function obterPrecoCustoProduto($codigoProduto,$xml){
+
+		foreach($xml->NFe->infNFe->det as $produto): //percorre todos os produtos
+			$codXML = $produto->prod->cProd; //captura codigo do produto
+
+			if($codigoProduto == $codXML){
+				return $produto->prod->vUnTrib; //retorna Preco Custo do produto
+			}
+
+		endforeach;
+	}//fim da funcao
+
 }//fim da classe
 
 ?>
