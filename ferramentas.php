536c66c03aed132dc3890a18ae70aefb18d7521c
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-09-10 18:30:35 -0300

    cirada a lista de ferramentas

---
+++ ferramentas.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<html>
+  <head>
+    <title>KIT FERRAMENTAS</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" type='text/css'>
+  </head>
+  <body class="container">
+    <div class="jumbotron">
+      <h1 class="display-4">Ferramentas</h1>
+      <hr class="my-4">
+    </div>
+    <div class="row">
+      <div class="col-sm-3"></div><!--Fim da Col-->
+      <div class="col-sm-3">
+        <p>
+          <button type="button" class="btn btn-primary">Adicionar Numero NF</button>
+        </p>
+        <p>
+          <button type="button" class="btn btn-primary">Adicionar Quantidade de Produtos</button>
+        </p>
+        <p>
+          <button type="button" class="btn btn-primary">Inserir o Código do Produto na Descrição</button>
+        </p>
+      </div><!--Fim da Col-->
+      <div class="col-sm-3"></div><!--Fim da Col-->
+    </div><!--Fim da Row-->
+  </body>
+</html>
